1557c9da5b3e493cacaa3ce2216753bf311d9dcd
alafa <ainhoalafuente011@gmail.com>
2021-01-10 13:02:44 +0100

    URL linking

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from projects import views
 
+app_name = "projects"
+
 urlpatterns = [
     path('', views.project_list),
-    path('<int:pk>', views.project_detail)
+    path('<int:pk>', views.project_detail, name="project_detail")
 ]
